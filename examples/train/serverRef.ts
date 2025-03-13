792e6798e495c80aca1c1396828c8370e73163cc
jmjf <jamee.mikell@gmail.com>
2025-03-13 22:04:35 +0000

    FEAT: ref server for train API

---
+++ examples/train/serverRef.ts
@@ -0,0 +1,54 @@
+import Fastify, { type FastifyReply, type FastifyRequest } from 'fastify';
+
+import { create_BookingRouteOptions } from './ror/create_BookingRouteOptions.ts';
+import { create_Booking_PaymentRouteOptions } from './ror/create_Booking_PaymentRouteOptions.ts';
+import { delete_BookingRouteOptions } from './ror/delete_BookingRouteOptions.ts';
+import { get_BookingRouteOptions } from './ror/get_BookingRouteOptions.ts';
+import { get_BookingsRouteOptions } from './ror/get_BookingsRouteOptions.ts';
+import { get_StationsRouteOptions } from './ror/get_StationsRouteOptions.ts';
+import { get_TripsRouteOptions } from './ror/get_TripsRouteOptions.ts';
+
+const fastify = Fastify({
+	logger: true,
+	ajv: {
+		customOptions: {
+			keywords: [
+				{
+					keyword: 'example',
+					errors: false,
+				},
+			],
+			formats: {
+				'iso-country-code': {
+					validate: (data: string) =>
+						['BE', 'DK', 'DE', 'ES', 'FR', 'IT', 'LU', 'NL', 'PT'].includes(data.toUpperCase()),
+				},
+			},
+		},
+	},
+});
+
+function requestHandler(request: FastifyRequest, reply: FastifyReply) {
+	const { id, url, method, query, body, params, headers } = request;
+	request.log.info({ id, url, method, query, body, params, headers }, 'RECEIVED');
+	reply.send();
+}
+
+fastify.route({ ...create_Booking_PaymentRouteOptions, handler: requestHandler });
+fastify.route({ ...create_BookingRouteOptions, handler: requestHandler });
+fastify.route({ ...delete_BookingRouteOptions, handler: requestHandler });
+fastify.route({ ...get_BookingRouteOptions, handler: requestHandler });
+fastify.route({ ...get_BookingsRouteOptions, handler: requestHandler });
+fastify.route({ ...get_StationsRouteOptions, handler: requestHandler });
+fastify.route({ ...get_TripsRouteOptions, handler: requestHandler });
+
+const start = async () => {
+	try {
+		await fastify.listen({ port: 3080, host: '0.0.0.0' });
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+};
+
+start();
