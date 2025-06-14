91dc3cc7bf667b38a90181aa910713421db12869
jmjf <jamee.mikell@gmail.com>
2025-06-14 17:38:10 +0000

    test: change fixtures for new test cases

---
+++ test/fixtures/tbr/headers_XDateHeader.ts
@@ -0,0 +1,7 @@
+import {type Static, Type, SchemaOptions, Clone, Kind, TypeRegistry, TSchema, TUnion} from "@sinclair/typebox"
+import { Value } from "@sinclair/typebox/value";
+
+
+
+export const XDateHeaderSchema = Type.Unsafe<Date|string>(Type.String({"format":"date"}))
+export type XDateHeader = Static<typeof XDateHeaderSchema>
